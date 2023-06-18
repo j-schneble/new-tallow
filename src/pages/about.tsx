db627904784cf5b2252ba3b4fffc4f00bf16021f
Jack Schneble <schneble.jack@gmail.com>
2023-06-17 22:05:32 -0400

    removed about page bg

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -45,7 +45,7 @@ export default function About() {
     <div className='flex '>
     <h2 className='mb-6 text-lg italic font-light text-white/75 font-sfprodisplayregular'>Founder, President</h2>
     </div>
-    <h1 className='flex items-center w-2/3 text-lg font-light tracking-wide text-left bg-zinc-950/50 text-white/90 font-sfprodisplayregular '>After 20 years in corporate technology and marketing, I decided to take my own career advice. I have always told my children to do what they love doing and then find a way to make money doing it. My father instilled a love of woodworking in me. I was the neighbor constantly doing woodworking projects like building wooden boats in my garage. So, I decided to start a new career building beautiful things for people.</h1>
+    <h1 className='flex items-center w-2/3 text-lg font-light tracking-wide text-left text-white/90 font-sfprodisplayregular '>After 20 years in corporate technology and marketing, I decided to take my own career advice. I have always told my children to do what they love doing and then find a way to make money doing it. My father instilled a love of woodworking in me. I was the neighbor constantly doing woodworking projects like building wooden boats in my garage. So, I decided to start a new career building beautiful things for people.</h1>
 </div>
 </div>
 
